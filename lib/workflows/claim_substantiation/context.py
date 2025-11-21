5304531576a3f2a213be88d740b3d265ae701077
Carlos Bonetti <carlosbonetti.mail@gmail.com>
2025-11-21 13:40:32 -0300

    refactor: transform worflow context to a pydantic model

--- lib/workflows/claim_substantiation/context.py
+++ lib/workflows/claim_substantiation/context.py
@@ -1,10 +1,17 @@
-from dataclasses import dataclass
 from typing import Optional
 
 from lib.services.vector_store import VectorStoreService
+from pydantic import BaseModel, ConfigDict, Field
 
 
-@dataclass
-class ContextSchema:
-    openai_api_key: Optional[str]
-    vector_store: VectorStoreService
+class ContextSchema(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    openai_api_key: Optional[str] = Field(
+        default=None,
+        description="The OpenAI API key to use for the workflow execution.",
+    )
+    vector_store: Optional[VectorStoreService] = Field(
+        default=None,
+        description="The vector store service to use for the workflow execution.",
+    )
